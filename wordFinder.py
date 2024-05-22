2e549b5134df5f2ee4234b9feb1308affeaab2f6
Jonioss <131784015+Jonioss@users.noreply.github.com>
2024-05-22 23:44:02 +0300

    Update wordFinder.py

--- wordFinder.py
+++ wordFinder.py
@@ -2,30 +2,39 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from random import choice
 
-words3 = []
-MAXGUESSES = 10
-def fetchWords(): ## Uses BeautifulSoup4 to download the list of 3-letter words
+url3 = 'https://scrabble.collinsdictionary.com/word-lists/three-letter-words-in-scrabble/'
+url4 = 'https://scrabble.collinsdictionary.com/word-lists/four-letter-words-in-scrabble/'
+url5 = 'https://scrabble.collinsdictionary.com/word-lists/five-letter-words-in-scrabble/'
+words = []
+MAXGUESSES = 7
+def fetchWords(n): ## Uses BeautifulSoup4 to download the list of 3-letter words
     print("Fetching list of words...")
     temp = []
-    url = 'https://scrabble.collinsdictionary.com/word-lists/three-letter-words-in-scrabble/'
+    url = ""
+    if n == 3:
+        url = url3
+    elif n == 4:
+        url = url4
+    elif n == 5:
+        url = url5
     page = urlopen(url)
     html = page.read().decode('utf-8')
     soup = BeautifulSoup(html, "html.parser")
     for line in soup.getText().split('\n'):
-        if len(line) == 3:
+        if len(line) == n:
             temp.append(line.lower()) ## Add the 3-letter word to the list
     print("Done.")
     return temp ## Return the list
-def play(): ## Play the game
+def play(n): ## Play the game
     guesses = 0
-    word = choice(words3) ## Get a random word from the list
+    word = choice(words) ## Get a random word from the list
     ## print(f"Hidden word is: {word}")
     while(guesses < MAXGUESSES):
         guess = str(input("Guess a word > "))
-        if(len(guess) != 3): ## If word isn't 3 letters long
+        if(len(guess) != n): ## If word isn't 3 letters long
             print("Invalid Guess. Try Again.")
             continue
-        if guess not in words3: ## If word isn't valid
+        if guess not in words: ## If word isn't valid
             print("This word doesn't exist in this database. Try another.")
             continue
         if(guess == word): ## If the user guesses the word
@@ -40,8 +49,13 @@ def play(): ## Play the game
                 print(f" -> {guess[i]}: Not in the word")
     return [0, word]
 
-words3 = fetchWords() ## Get the list of words.
-result, word = play() ## Play the game. Result = 0 means the user lost, result = 1 means the user won.
+
+n = int(input("Number of letters in each word(3, 4 or 5): ")) ## words will contain n letters
+if n not in [3, 4, 5]:
+    print("Invalid number.")
+    exit()
+words = fetchWords(n) ## Get the list of words.
+result, word = play(n) ## Play the game. Result = 0 means the user lost, result = 1 means the user won.
 
 ## Print appropriate game result
 if(result == 0):
